b7bb62fca374936044e7bf2e9d9d93406e950bc3
Brandon Clark <B.Y.Clark@dundee.ac.uk>
2021-10-01 11:03:44 +0100

    more minor testing

--- src/App.js
+++ src/App.js
@@ -18,14 +18,14 @@ function App() {
   let userStateStart = ""
   let idStateStart = ""
   let positionStateStart = ""
-  if (!!Cookies.get('access_token')) {
-    userStateStart = Cookies.get('access_token').split("#")[0]
-    idStateStart = Cookies.get('access_token').split("#")[1]
-    positionStateStart = Cookies.get('access_token').split("#")[2]
-  }
+  // if (!!Cookies.get('access_token')) {
+  //   userStateStart = Cookies.get('access_token').split("#")[0]
+  //   idStateStart = Cookies.get('access_token').split("#")[1]
+  //   positionStateStart = Cookies.get('access_token').split("#")[2]
+  // }
 
-  const [user, setUser] = useState({user: userStateStart, id: idStateStart, position: positionStateStart})
-  console.log(user);
+  // const [user, setUser] = useState({user: userStateStart, id: idStateStart, position: positionStateStart})
+  // console.log(user);
 
     let LandingWrapper = (props) => {
       return( 
