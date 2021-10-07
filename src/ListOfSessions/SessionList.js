73a085e4c521bba29a6252ff0d93266b9bb28d47
skyekirwan <scoobyskye@aol.com>
2021-10-07 12:51:39 +0100

    POST request to Flask session endpoint

--- src/ListOfSessions/SessionList.js
+++ src/ListOfSessions/SessionList.js
@@ -2,13 +2,32 @@ import React, { Component } from 'react'
 import './SessionList.css';
 
 export default class SessionList extends Component{
+  constructor(props){
+      super(props);
+  }
 
-    render() {
-        return (
-            <div className = "sessionlist-wrapper">
-                <h1>List of Sessions</h1> 
+  getUserSessions = e =>{
+      e.preventDefault();
+      //POSTING request with userID
+      const requestOptions = {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ userID: String(localStorage.getItem('userID'))})
+      };
+      fetch('http://localhost:5000/sessions', requestOptions)
+              // JSON response is handled by a json() promises
+      .then((res) => { return res.json().
+      then((data) => {
+          console.log(data)
+      });
+      });
+  }
+  render() {
+      return (
+          <div className = "sessionlist-wrapper">
+              <h1>List of Sessions</h1>
 
-            </div>
-        )
-    }
+          </div>
+      )
+  }
 }
--- src/ViewClientPage/ViewClientPage.js
+++ src/ViewClientPage/ViewClientPage.js
@@ -7,12 +7,12 @@ import {
     Route,
     Link
   } from "react-router-dom";
-  
+
 import './ViewClientPage.css';
 import Settings from "../Settings/Settings.js";
 
 let settings = (props) => {
-    return( 
+    return(
       <Settings></Settings>
     )
   }
@@ -21,7 +21,7 @@ export default class ViewClientPage extends Component{
   constructor(props){
     super(props);}
     settings = (props) => {
-        return( 
+        return(
           <Settings></Settings>
         )
       }
@@ -36,9 +36,9 @@ export default class ViewClientPage extends Component{
 
                             <title>Userpage</title>
                         </Helmet>
-                        <Link to ="/userpage">My Home</Link>  
-                        <Link to ="/settings">Settings</Link>        
-                       
+                        <Link to ="/userpage">My Home</Link>
+                        <Link to ="/settings">Settings</Link>
+
                     </div>
                     <div className ="mypage-body">
                         <Route path = "/settings" component={settings} />
