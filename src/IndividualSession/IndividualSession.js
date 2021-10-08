14fc134747fdab1ffb7ad183e8310521933f23b2
skyekirwan <scoobyskye@aol.com>
2021-10-08 11:22:56 +0100

    POST request to Flask for sensor data from session

--- src/IndividualSession/IndividualSession.js
+++ src/IndividualSession/IndividualSession.js
@@ -2,11 +2,33 @@ import React, { Component } from 'react'
 import './IndividualSession.css';
 
 export default class IndividualSession extends Component{
+  constructor(props){
+      super(props);
+      this.state = {
+            sensors: []
+        };
+  }
+  // comonentDidMount part of React lifecycle - runs automatically
+  componentDidMount() {
+    //POSTING request with userID
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ sessionID: String(localStorage.getItem('sessionID'))})
+    };
+    fetch('http://localhost:5000/sensors', requestOptions)
+            // JSON response is handled by a json() promises
+    .then((res) => { return res.json().
+    then((data) => {
+        console.log(data)
+    });
+    });
+  }
 
     render() {
         return (
             <div className = "indivsession-wrapper">
-                <h1>Your Session</h1> 
+                <h1>Your Session</h1>
 
             </div>
         )
