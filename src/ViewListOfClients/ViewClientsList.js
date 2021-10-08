ad902831c227c4f60ec4069100e40635387f5624
skyekirwan <scoobyskye@aol.com>
2021-10-08 16:00:53 +0100

    Requesting and showing a physios client list

--- src/ViewListOfClients/ViewClientsList.js
+++ src/ViewListOfClients/ViewClientsList.js
@@ -2,12 +2,42 @@ import React, { Component } from 'react'
 import './ViewClientsList.css';
 
 export default class ViewClientList extends Component{
+  constructor(props){
+      super(props);
+      this.state = {
+            clients: []
+        };
+  }
+  // comonentDidMount part of React lifecycle - runs automatically
+  componentDidMount() {
+    //POSTING request with userID
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ userID: String(localStorage.getItem('userID'))})
+    };
+    fetch('https://theobackend.herokuapp.com/clients', requestOptions)
+            // JSON response is handled by a json() promises
+    .then((res) => { return res.json().
+    then((data) => {
+        this.setState({clients: data});
+    });
+    });
+  }
 
-    render() {
-        return (
-            <div className = "clientlist-wrapper">
-                
-            </div>
-        )
-    }
+  render() {
+      return (
+          <div className = "clientlist-wrapper p-5">
+              <h1>Your Clients</h1>
+                <ListGroup className="mt-5">
+                  <ListGroup.Item action href="#link1">
+                  {JSON.stringify(this.state.clients[0])}
+                  </ListGroup.Item>
+                  <ListGroup.Item action href="#link2">
+                  {JSON.stringify(this.state.clients[0])}
+                  </ListGroup.Item>
+                </ListGroup>
+          </div>
+      )
+  }
 }
