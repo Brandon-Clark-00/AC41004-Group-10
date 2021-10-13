eeb30ebb740abd77c92114dee6e4cee1e17db1f2
dillonreid15 <dillonreid15@gmail.com>
2021-10-13 16:23:43 +0100

    Mapped data to graph

--- src/IndividualSession/IndividualSession.js
+++ src/IndividualSession/IndividualSession.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom';
 import Helmet from 'react-helmet';
+import { XYPlot, XAxis, YAxis, VerticalGridLines, HorizontalGridLines, VerticalBarSeries, VerticalBarSeriesCanvas, DiscreteColorLegend } from 'react-vis';
 import './IndividualSession.css';
+
+const myData = [
+    {x: 'A', y: 10},
+    {x: 'B', y: 5},
+    {x: 'C', y: 15}
+  ]
 
 export default class IndividualSession extends Component{
   constructor(props){
@@ -38,6 +45,80 @@ export default class IndividualSession extends Component{
                     <NavLink to ="/user/home" className="sessionlink" exact>
                         <li>Back to Sessions</li>
                     </NavLink>    
+                </div>
+                <div className="chart-container">
+                    <XYPlot className="individual-session-bar-chart"
+                    xType="ordinal"
+                    stackBy="y"
+                    width={300}
+                    height={300}>
+                        <XAxis />
+                        <YAxis />
+                        <VerticalBarSeries 
+                                    cluster="2015"
+                                    color="#79C7E3"
+                                    data={[
+                                      {x: 'Q1', y: 3},
+                                      {x: 'Q2', y: 7},
+                                      {x: 'Q3', y: 2},
+                                      {x: 'Q4', y: 1}
+                                    ]}
+                        />
+                    </XYPlot>
+                    <XYPlot className="individual-session-bar-chart"
+                    xType="ordinal"
+                    stackBy="y"
+                    width={300}
+                    height={300}>
+                        <XAxis />
+                        <YAxis />
+                        <VerticalBarSeries 
+                                    cluster="2015"
+                                    color="#79C7E3"
+                                    data={[
+                                      {x: 'Q1', y: 3},
+                                      {x: 'Q2', y: 7},
+                                      {x: 'Q3', y: 2},
+                                      {x: 'Q4', y: 1}
+                                    ]}
+                        />
+                    </XYPlot>
+                    <XYPlot className="individual-session-bar-chart"
+                    xType="ordinal"
+                    stackBy="y"
+                    width={300}
+                    height={300}>
+                        <XAxis />
+                        <YAxis />
+                        <VerticalBarSeries 
+                                    cluster="2015"
+                                    color="#79C7E3"
+                                    data={[
+                                      {x: 'Q1', y: 3},
+                                      {x: 'Q2', y: 7},
+                                      {x: 'Q3', y: 2},
+                                      {x: 'Q4', y: 1}
+                                    ]}
+                        />
+                    </XYPlot>
+                    <XYPlot className="individual-session-bar-chart"
+                    xType="ordinal"
+                    stackBy="y"
+                    width={300}
+                    height={300}>
+                        <XAxis />
+                        <YAxis />
+                        <VerticalBarSeries 
+                                    cluster="2015"
+                                    color="#79C7E3"
+                                    data={[
+                                      {x: 'Q1', y: 3},
+                                      {x: 'Q2', y: 7},
+                                      {x: 'Q3', y: 2},
+                                      {x: 'Q4', y: 1}
+                                    ]}
+                        />
+                    </XYPlot>
                 </div>
                 <h1>Session</h1>
                 <p style={{fontSize:"0.5em"}}> {JSON.stringify(this.state.sensors[0])} </p>
