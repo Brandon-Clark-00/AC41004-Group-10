a0e7b3518d14b2f206c1a480ce43b3eff1fa0d1c
Brandon <byclark@dundee.ac.uk>
2021-10-14 11:07:18 +0100

    Fixed for 1920x1080

--- src/LiveHeatmap/LiveHeatmap.js
+++ src/LiveHeatmap/LiveHeatmap.js
@@ -237,7 +237,7 @@ export default class LiveHeatmap extends Component {
         console.log(event);
       });
     }
-    this.pauseButton.position(0 * scale, 0 * scale, "relative").parent(divBox);
+    this.pauseButton.position(-615 * scale, 30 * scale, "relative").parent(divBox);
     this.pauseButton.addClass("button")
     this.timeSlider.position(150 * scale, -20 * scale, "relative").parent(divBox);
     this.timeSlider.addClass("slider")
