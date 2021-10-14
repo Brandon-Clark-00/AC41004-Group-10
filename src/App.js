9312152c5724da21b2e0fd80f06e14a3780a94a3
Brandon <byclark@dundee.ac.uk>
2021-10-14 17:15:40 +0100

    Fixed all the issues with merge

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Cookies from 'js-cookie'
 import {
   BrowserRouter as Router,
@@ -6,6 +6,14 @@ import {
   Redirect,
   Switch
 } from "react-router-dom";
+
+
+import styled, { ThemeProvider } from "styled-components";
+import { GlobalStyles } from './theme/GlobalStyles';
+import {useTheme} from './theme/useTheme';
+import ThemeSelector from './ThemeSelector';
+import * as themes from './theme/schema.json';
+
 
 import Landing from "./LandingPage/Login.js";
 import ClientPage from './ViewListOfClients/ClientPage.js';
@@ -28,93 +36,81 @@ function App() {
     positionStateStart = Cookies.get('access_token').split("#")[2]
   }
 
-{/*
-  const[getMessage, setGetMessage] = useState({})
-  useEffect(()=>{
-    Axios.get('https://theobackend.herokuapp.com/users').then(response => {
-      console.log("SUCCESS", response)
-      setGetMessage(response)
-    }).catch(error => {
-      console.log("Something is wrong", error)
-    })
-  }, [])
-*/}
-  const [user, setUser] = useState({user: userStateStart, id: idStateStart, position: positionStateStart})
+  const [user, setUser] = useState({ user: userStateStart, id: idStateStart, position: positionStateStart })
   console.log(user);
 
-    let LandingWrapper = (props) => {
-      return(
-        <Landing> history={props.history} user={user} userCallback={(a) => setUser(a)}</Landing>
-      )
-    }
+  const {theme, themeLoaded,} = useTheme();
+  const [selectedTheme, setSelectedTheme] = useState(theme);
 
-    let ViewClientPageWrapper = (props) => {
-      return(
-        <ViewClientPage></ViewClientPage>
-      )
-    }
+  useEffect(() => {
+    setSelectedTheme(theme);
+  }, [themeLoaded]);
 
-    let ViewListOfClientsWrapper = (props) => {
-      return(
-        <ViewListOfClients></ViewListOfClients>
-      )
-    }
+  let LandingWrapper = (props) => {
+    return (
+      <Landing> history={props.history} user={user} userCallback={(a) => setUser(a)}</Landing>
+    )
+  }
 
+  let ViewClientPageWrapper = (props) => {
+    return (
+      <ViewClientPage></ViewClientPage>
+    )
+  }
+
+  let ViewListOfClientsWrapper = (props) => {
+    return (
+      <ViewListOfClients></ViewListOfClients>
+    )
+  }
 
   return (
+
     <Router>
       <>
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css"
-      integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU"
-      crossorigin="anonymous"
-      />
-      { 
-      themeLoaded && <ThemeProvider theme={ selectedTheme }>
-              <GlobalStyles/>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css"
+          integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU"
+          crossorigin="anonymous"
+        />
+        {
+          themeLoaded && <ThemeProvider theme={selectedTheme}>
+            <GlobalStyles />
 
-      <div className='page-container'>
-          <header id="site-header" className="site-header">
+            <div className='page-container' style = {{fontFamily: selectedTheme.font}}>
+            <header id="site-header" className="site-header">
             <img className="theohealthlogo" src={theohealthlogo} alt="Brand Logo"/>
             </header>
-          <div className="main">
+
+            <div className="main">
+
             <Switch>
-              <Route exact path="/" component={LandingWrapper}/>
-              <Route exact path="/physio/clientlist" component={ViewListOfClientsWrapper}/>
-              <Route exact path="/user/home" component={ViewClientPageWrapper}/>
-              <Route path ="/user/session" component={IndividualSession} />
-              <Route path = "/physio/client" component={ClientPage} />
-              <Redirect to ="/"></Redirect>
+            <Route exact path="/" component={LandingWrapper}/>
+            <Route exact path="/physio/clientlist" component={ViewListOfClientsWrapper}/>
+            <Route exact path="/user/home" component={ViewClientPageWrapper}/>
+            <Route path ="/user/session" component={IndividualSession}/>
+            <Redirect to ="/"></Redirect>
+            </Switch>
 
-                <div className="main">
+            </div>
 
-                  <Switch>
-                    <Route exact path="/" component={LandingWrapper}/>
-                    <Route exact path="/physio/clientlist" component={ViewListOfClientsWrapper}/>
-                    <Route exact path="/user/home" component={ViewClientPageWrapper}/>
-                    <Route path ="/user/session" component={IndividualSession}/>
-                    <Redirect to ="/"></Redirect>
-                  </Switch>
+            <footer className="site-footer">
 
-                </div>
+            <div className="social-media">
+            <a href="https://twitter.com/theoHealth"><i class ="fa fa-twitter" title = "Theo Health Twitter"></i></a>
+            <a href="https://www.instagram.com/theo_health/"><i class ="fa fa-instagram" title = "Theo Health Instagram"></i></a>
+            <a href="https://www.linkedin.com/company/theo-health"><i class ="fa fa-linkedin" title = "Theo Health LinkedIn"></i></a>
+            <a href="mailto:jodie@theohealth.com"><i class ="fa fa-envelope" title = "Theo Health Email"></i></a>
+            </div>
 
-                <footer className="site-footer">
-
-                  <div className="social-media">
-                    <a href="https://twitter.com/theoHealth"><i class="fa fa-twitter" title = "Theo Health Twitter"></i></a>
-                    <a href="https://www.instagram.com/theo_health/"><i class="fa fa-instagram" title = "Theo Health Instagram"></i></a>
-                    <a href="https://www.linkedin.com/company/theo-health"><i class="fa fa-linkedin" title = "Theo Health LinkedIn"></i></a>
-                    <a href="mailto:jodie@theohealth.com"><i class="fa fa-envelope" title = "Theo Health Email"></i></a>
-                  </div>
-
-                </footer>
+            </footer>
             </div>
           </ThemeProvider>
         }
-        </>
-      </Router>
-      
+      </>
+    </Router>
+
   );
 }
 
